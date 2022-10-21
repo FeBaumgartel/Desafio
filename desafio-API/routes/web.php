fc957e8e31aa50666f3d5059d2be27acf53eb920
Felipe Baumgartel <baumgartel@multiplier.com.br>
2022-10-21 12:34:19 -0300

    feat: created routes

--- desafio-API/routes/web.php
+++ desafio-API/routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('product')->group(function () {
+    Route::get('/', 'ProductController@get');
+    Route::get('/{id}', 'ProductController@find');
+    Route::post('/', 'ProductController@create');
+    Route::patch('/{id}', 'ProductController@edit');
+});
+Route::prefix('cart')->group(function () {
+    Route::get('/', 'CartController@get');
+    Route::get('/{id}', 'CartController@find');
+    Route::post('/', 'CartController@create');
+    Route::post('/{id}', 'CartController@addProduct');
+});
+Route::prefix('order')->group(function () {
+    Route::get('/', 'OrderController@get');
+    Route::get('/{id}', 'OrderController@find');
+    Route::post('/', 'OrderController@create');
 });
